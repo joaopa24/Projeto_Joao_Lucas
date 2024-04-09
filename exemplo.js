a5750460ee9876f2d41b4822f6b88d50909adb1d
joknz21 <159097224+joknz21@users.noreply.github.com>
2024-04-09 09:56:00 -0300

    Add files via upload

---
+++ exemplo.js
@@ -0,0 +1,8 @@
+// Declaração de uma função chamada helloWorld
+function helloWorld() {
+  // Imprime a mensagem "Olá, mundo!" no console do navegador
+  console.log("Olá, mundo!");
+}
+
+// Chamada da função helloWorld
+helloWorld();
