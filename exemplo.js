36e13d5eb4fded630885ac7c9985f292f4a7da74
joknz21 <joaokn129@gmail.com>
2024-04-09 20:39:56 -0300

    NewFeature

--- exemplo.js
+++ exemplo.js
@@ -1,8 +1,76 @@
-// Declaração de uma função chamada helloWorld
-function helloWorld() {
-  // Imprime a mensagem "Olá, mundo!" no console do navegador
-  console.log("Olá, mundo!");
+// Definição da classe A
+class A {
+  constructor(A1, A2) {
+    this._A1 = A1;
+    this._A2 = A2;
+  }
+
+  // Getters e Setters
+  get A1() {
+    return this._A1;
+  }
+
+  set A1(value) {
+    this._A1 = value;
+  }
+
+  get A2() {
+    return this._A2;
+  }
+
+  set A2(value) {
+    this._A2 = value;
+  }
+
+  // Métodos MA1 e MA2
+  MA1() {
+    console.log("Método MA1 chamado");
+  }
+
+  MA2() {
+    console.log("Método MA2 chamado");
+  }
 }
 
-// Chamada da função helloWorld
-helloWorld();
+// Definição da classe B
+class B {
+  constructor(B1, B2) {
+    this._B1 = B1;
+    this._B2 = B2;
+  }
+
+  // Getters e Setters
+  get B1() {
+    return this._B1;
+  }
+
+  set B1(value) {
+    this._B1 = value;
+  }
+
+  get B2() {
+    return this._B2;
+  }
+
+  set B2(value) {
+    this._B2 = value;
+  }
+
+  // Métodos MB1 e MB2
+  MB1() {
+    console.log("Método MB1 chamado");
+  }
+
+  MB2() {
+    console.log("Método MB2 chamado");
+  }
+}
+
+// Exemplo de utilização das classes A e B
+const objetoA = new A(10, 3.14);
+const objetoB = new B(20, 6.28);
+
+objetoA.MA1();
+objetoA.MA2();
+objetoB.MB1();
+objetoB.MB2();
